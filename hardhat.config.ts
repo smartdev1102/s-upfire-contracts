fc5951dc3a5777eb0da37f2490c411682f88dbfb
SunlightLuck <sunlight.luck@gmail.com>
2022-12-07 00:11:06 -0500

    bug fixed

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -49,11 +49,13 @@ const config: HardhatUserConfig = {
       chainId: 43114,
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    bsctest: {
+      url: "https://data-seed-prebsc-2-s2.binance.org:8545",
+      chainId: 97,
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     }
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
